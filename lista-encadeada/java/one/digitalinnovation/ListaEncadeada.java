cff294eb180667318ab4c1cb2a1792b9f286c219
Luiz Bottino <luizbottino@outlook.com>
2023-03-07 19:07:37 -0300

    Criação da lista encadeada e da classe No.

---
+++ lista-encadeada/java/one/digitalinnovation/ListaEncadeada.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation;
+
+public class ListaEncadeada<T> {
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada(){
+        this.referenciaEntrada = null;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+
+        if (this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for (int i = 0; i < this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
+    public int size(){
+        int tamanhoLista = 0;
+
+        No<T> referenciaAuxiliar = referenciaEntrada;
+
+        while (true){
+            if (referenciaAuxiliar != null){
+                tamanhoLista++;
+                if (referenciaAuxiliar.getProximoNo() != null){
+                    referenciaAuxiliar = referenciaAuxiliar.getProximoNo();
+                }else {
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+
+        return tamanhoLista;
+    }
+    public boolean isEmpty(){
+        return referenciaEntrada == null ? true : false;
+    }
+}
